9be919d3c94d54765f6a955104e21809e347288b
PRIYANSHU KESHARWANI <134344291+Priyanshu88585@users.noreply.github.com>
2026-03-13 19:32:34 +0530

    Update route.ts

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -1,25 +1,25 @@
-import { NextRequest } from "next/server"
-import { getBlogs, createBlog } from "@/services/blog.service"
-import { successResponse, errorResponse } from "@/lib/api"
+// import { NextRequest } from "next/server"
+// import { getBlogs, createBlog } from "@/services/blog.service"
+// import { successResponse, errorResponse } from "@/lib/api"
 
-export async function GET() {
-  try {
-    const blogs = await getBlogs()
+// export async function GET() {
+//   try {
+//     const blogs = await getBlogs()
 
-    return successResponse(blogs)
-  } catch {
-    return errorResponse("Failed to fetch blogs")
-  }
-}
+//     return successResponse(blogs)
+//   } catch {
+//     return errorResponse("Failed to fetch blogs")
+//   }
+// }
 
-export async function POST(req: NextRequest) {
-  try {
-    const body = await req.json()
+// export async function POST(req: NextRequest) {
+//   try {
+//     const body = await req.json()
 
-    const blog = await createBlog(body)
+//     const blog = await createBlog(body)
 
-    return successResponse(blog, "Blog created")
-  } catch {
-    return errorResponse("Blog creation failed")
-  }
-}
+//     return successResponse(blog, "Blog created")
+//   } catch {
+//     return errorResponse("Blog creation failed")
+//   }
+// }
